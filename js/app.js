0e541b01b12fb2fd3a742e8895796a455595490a
DashRock <dash.lalonde@protonmail.com>
2020-02-28 19:28:49 -0800

    Structural Layout

---
+++ js/app.js
@@ -0,0 +1,4 @@
+document.getElementById('headerNavBtn-3').addEventListener('click', () => {
+    document.getElementById('examplesInfo').style.display = "grid";
+    document.getElementById('brandInfo').style.display = "none";
+})
